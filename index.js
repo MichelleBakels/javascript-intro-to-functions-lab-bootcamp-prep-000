eac6f5f59d38a1fecf072d25865fa46ca8f5feda
Michelle Bakels <michelle.bakels@gmail.com>
2018-01-17 02:13:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,20 @@ function shout(string) {
 function whisper(string){
   return string.toLowerCase()
 }
+
+function logShout(string){
+  console.log(toUpperCase)
+}
+
+function logWhisper(string){
+  console.log(toLowerCase)
+}
+
+function sayHiToGrandma(string){
+  if (string.toLowerCase)
+    return "I can't hear you!"
+  elseif (string.toUpperCase)
+    return "YES INDEED!"
+  elseif (string === "I love you, Grandma")
+    return "I love you, too."
+}
